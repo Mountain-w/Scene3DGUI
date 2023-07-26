37e9adbea30f555b2ca7fa2fae83025afabdbbdc
wangrui <wangrui@mindflow.com.cn>
2023-07-26 22:49:41 +0800

    begin

--- Scene3DGUI/Scene3DGUI/PointCloudService.cpp
+++ Scene3DGUI/Scene3DGUI/PointCloudService.cpp
@@ -2,19 +2,6 @@
 
 bool PointCloudService::getPcdType(const char* pcdPath, PCDTYPE& _type)
 {
-	pcl::PCDReader _reader;
-	pcl::PCDHeader _header;
-
-	if (_reader.readHeader(pcdPath) != 0)
-	{
-		return false;
-	}
-	std::cout << "Version:" << _header.version << std::endl;
-	std::cout << "Fields:" << _header.fields.size() << std::endl;
-	std::cout << "Width:" << _header.width << std::endl;
-	std::cout << "Height:" << _header.height << std::endl;
-	std::cout << "PointNum:" << _header.point_count << std::endl;
-
 	return true;
 };
 
